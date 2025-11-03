e063dbaaf1289bcbde9500c1ccabc08220dad0c8
Saurav Kumar <csaurav2014@gmail.com>
2025-11-03 21:38:06 +0530

    Implement and Practice of different type of Inter-Service Communication in Microservices like RestTemplate, OpenFeign, RestClient, WebClient

---
+++ additional/interservice/consumer/src/main/java/com/demo/consumer/restclient/RestClientController.java
@@ -0,0 +1,27 @@
+package com.demo.consumer.restclient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/api/rest-client")
+@RequiredArgsConstructor
+public class RestClientController {
+
+    private final ProviderRestClient providerRestClient;
+
+    @GetMapping("/instance")
+    public String getInstanceInfo(){
+      /*  RestClient restClient  = RestClient.create();
+        String response = restClient.get()
+                .uri("http://localhost:8081/instance-info")
+                .retrieve()
+                .body(String.class);
+        return response;*/
+
+        return providerRestClient.getInstanceInfo();
+    }
+}
