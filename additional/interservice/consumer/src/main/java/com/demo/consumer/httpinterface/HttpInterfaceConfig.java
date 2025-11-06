d7bc5c338499e0686a486ffb76ee00b193716dd6
Saurav Kumar <csaurav2014@gmail.com>
2025-11-06 21:59:32 +0530

    Implement service calls using @LoadBalanced HTTP Interfaces
    - Defines a @HttpExchange interface to declare provider endpoints.
    - Configures the client to be @LoadBalanced for Eureka discovery.
    - Resolves potential MultipleBeanDefinitionsException by clarifying
      which WebClient.Builder (or other bean) is @Primary.
    
    This work is supported by a deep-dive into Eureka's internal
    resilience model, including:
    - Heartbeat mechanisms
    - Graceful shutdown procedures
    - Configuration of Self-Preservation Mode

--- additional/interservice/consumer/src/main/java/com/demo/consumer/httpinterface/HttpInterfaceConfig.java
+++ additional/interservice/consumer/src/main/java/com/demo/consumer/httpinterface/HttpInterfaceConfig.java
@@ -1,5 +1,8 @@
 package com.demo.consumer.httpinterface;
 
+import com.demo.consumer.restclient.RestClientConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -9,7 +12,11 @@ import org.springframework.web.reactive.function.client.support.WebClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class HttpInterfaceConfig {
+
+    // Consume bean from RestClientConfig file.
+    //private final RestClientConfig config;
 
     /*@Bean
     public ProviderHttpInterface webClientHttpInterface(){
@@ -23,10 +30,16 @@ public class HttpInterfaceConfig {
         return service;
     }*/
 
-    @Bean
-    public ProviderHttpInterface restClientHttpInterface(){
+   /* @Bean
+    @LoadBalanced
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }*/
 
-        RestClient restClient = RestClient.builder().baseUrl("http://localhost:8081").build();
+    @Bean
+    public ProviderHttpInterface restClientHttpInterface(RestClient.Builder restClientBuilder) {
+
+        RestClient restClient = restClientBuilder.baseUrl("http://provider").build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
 
